e26dbed913e1c7e6f8905e7251e585f2333cf301
wsYYnb3 <danielisraeli033@gmail.com>
2023-07-12 13:28:04 +0200

    intiial commit

---
+++ movies/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router } from 'react-router-dom';  // <- Import Router here
+import Header from './components/Header';
+import AppRoutes from './AppRoutes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Container>
+          <AppRoutes />
+        </Container>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ movies/src/AppRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import HistoryPage from './pages/HistoryPage';
+import LoginPage from './pages/LoginPage';
+import SearchPage from './pages/SearchPage';
+import MovieDetailsPage from './pages/MovieDetailsPage';
+import SignUpPage from './pages/SignUpPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function AppRoutes() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/movies/:id" element={<MovieDetailsPage />} />
+      <Route path="/history" element={<HistoryPage />} />
+      <Route path="*" element={<h1>404: page not found</h1>} />
+    </Routes>
+  );
+}
