36babd20fd162860d4e1c7c0ce0290cade27c921
Fabio Gorodscy <fabiogo@outlook.com>
2023-07-13 08:39:31 -0300

    Uncommented header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router } from 'react-router-dom';  // <- Import Router here
-//import Header from './components/Header';
+import Header from './components/Header';
 import AppRoutes from './AppRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
@@ -11,12 +11,12 @@ function App() {
     <div className="App">
       <Router>
         <Container>
+          <Header />
           <AppRoutes />
         </Container>
       </Router>
     </div>
   );
-        //<Header />
 }
 
 export default App;
